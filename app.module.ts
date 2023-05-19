945798611caeb6377588444389988f4e43f8813a
JayanthR3 <jayanthr1664@gmail.com>
2023-05-19 14:45:08 +0530

    first commit

---
+++ app.module.ts
@@ -0,0 +1,23 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroFormComponent,
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
